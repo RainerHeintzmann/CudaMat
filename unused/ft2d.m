80f08d86bdb88b8055b22b1f7e68c70611084331
Rainer Heintzmann <heintzmann@gmail.com>
2019-09-21 09:04:48 +0200

    Bug fixes

---
+++ unused/ft2d.m
@@ -0,0 +1,29 @@
+% ft3d(in) DipImage style Fourier transforms cuda dat (up to 3D)
+
+%************************** CudaMat ****************************************
+%   Copyright (C) 2008-2009 by Rainer Heintzmann                          *
+%   heintzmann@gmail.com                                                  *
+%                                                                         *
+%   This program is free software; you can redistribute it and/or modify  *
+%   it under the terms of the GNU General Public License as published by  *
+%   the Free Software Foundation; Version 2 of the License.               *
+%                                                                         *
+%   This program is distributed in the hope that it will be useful,       *
+%   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+%   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+%   GNU General Public License for more details.                          *
+%                                                                         *
+%   You should have received a copy of the GNU General Public License     *
+%   along with this program; if not, write to the                         *
+%   Free Software Foundation, Inc.,                                       *
+%   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+%**************************************************************************
+%
+function out=ft2d(in)
+out=cuda();
+if isa(in,'cuda') 
+    out.ref=cuda_cuda('fftnd',in.ref,2);
+else
+    error('ft3d: Unsupported datatype');
+end
+out.fromDip=in.fromDip;
