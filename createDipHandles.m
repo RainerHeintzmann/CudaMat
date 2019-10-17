f944afbee9f78d1d5818b17a9e11f00d1794bf16
Rainer Heintzmann <heintzmann@gmail.com>
2019-10-17 18:16:46 +0200

    Updates and fixes

--- createDipHandles.m
+++ createDipHandles.m
@@ -1,17 +1,5 @@
 % function createDipHandles  : creates a number of handles to overwritten dipimage functions
 function createDipHandles()
-
-pathOld=pwd;  % remember where the current path is
-pathDIP=fileparts(which('dipimage/xx')); % get the directory path of the dipimage/xx function
-cd(pathDIP);  % go to the DipImage path to shadow the current cudaMat xx function.
-
-%[pathstr,name,ext]=fileparts(mfilename('fullpath'));
-% pathadd=1;
-% try
-%     rmpath(pathstr);   % remove the current CudaMat path
-% catch me
-%     pathadd=0;
-% end
 global diphandle_xx;
 global diphandle_yy;
 global diphandle_zz;
@@ -21,16 +9,38 @@ global diphandle_phiphi;
 global diphandle_newim;
 global diphandle_newimar;
 
-diphandle_xx=@xx;
-diphandle_yy=@yy;
-diphandle_zz=@zz;
-diphandle_rr=@rr;
-diphandle_ramp=@ramp;
-diphandle_phiphi=@phiphi;
-diphandle_newim=@newim;
-diphandle_newimar=@newimar;
+if 0 % isdeployed()==0
+    pathOld=pwd;  % remember where the current path is
+    pathDIP=fileparts(which('dipimage/xx')); % get the directory path of the dipimage/xx function
+    cd(pathDIP);  % go to the DipImage path to shadow the current cudaMat xx function.
+    diphandle_xx=@xx;
+    diphandle_yy=@yy;
+    diphandle_zz=@zz;
+    diphandle_rr=@rr;
+    diphandle_ramp=@ramp;
+    diphandle_phiphi=@phiphi;
+    diphandle_newim=@newim;
+    diphandle_newimar=@newimar;
+    cd(pathOld);  % return to where the user was before
+else
+    diphandle_xx=@xx_dip;
+    diphandle_yy=@yy_dip;
+    diphandle_zz=@zz_dip;
+    diphandle_rr=@rr_dip;
+    diphandle_ramp=@ramp_dip;
+    diphandle_phiphi=@phiphi_dip;
+    diphandle_newim=@newim_dip;
+    diphandle_newimar=@newimar_dip;
+end
+%[pathstr,name,ext]=fileparts(mfilename('fullpath'));
+% pathadd=1;
+% try
+%     rmpath(pathstr);   % remove the current CudaMat path
+% catch me
+%     pathadd=0;
+% end
 
-cd(pathOld);  % return to where the user was before
+
 
 %feval(diphandle_xx)
 % if pathadd
